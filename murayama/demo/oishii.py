63e21f9d3058570d366f6d63066cf0ebe759fa1c
murayama-natsuki <mercucumber@gmail.com>
2017-11-19 16:23:19 +0900

    revise

--- murayama/demo/oishii.py
+++ murayama/demo/oishii.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
+#python oshare.py hogehoge.mpdel [search word]
+#おいしい系と相関があるかをみてくれます
 
 from gensim.models import word2vec
 import sys
 
 model   = word2vec.Word2Vec.load(sys.argv[1])
-results = model.most_similar(positive=sys.argv[2], topn=1000)
-
-#results = model.most_similar(positive=['美味しい'] , topn= 1000)
-#oshare = [result for result in results if result[0]=="おしゃれ"]
-#print(oshare)
+results = model.most_similar(positive=sys.argv[2], topn=100)
+#results = model.most_similar(positive=['おしゃれ','オシャレ'] , topn= 1000)
 
 for result in results:
-	if result[0] == "おしゃれ":
+	if result[0] == "美味しい":
 		print(result[0], '\t', result[1])
-	if result[0] == "オシャレ":
-		print(result[0], '\t', result[1])
-	if result[0] == "お洒落":
+	if result[0] == "おいしい":
 		print(result[0], '\t', result[1])
 	else:
 		pass
--- murayama/demo/oshare.py
+++ murayama/demo/oshare.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
+#python oishii.py hogehoge.mpdel [search word]
+#おしゃれ系と相関があるかをみてくれます
 
 from gensim.models import word2vec
 import sys
 
 model   = word2vec.Word2Vec.load(sys.argv[1])
-results = model.most_similar(positive=sys.argv[2], topn=100)
-#results = model.most_similar(positive=['おしゃれ','オシャレ'] , topn= 1000)
+results = model.most_similar(positive=sys.argv[2], topn=1000)
+
+#results = model.most_similar(positive=['美味しい'] , topn= 1000)
+#oshare = [result for result in results if result[0]=="おしゃれ"]
+#print(oshare)
 
 for result in results:
-	if result[0] == "美味しい":
+	if result[0] == "おしゃれ":
 		print(result[0], '\t', result[1])
-	if result[0] == "おいしい":
+	if result[0] == "オシャレ":
+		print(result[0], '\t', result[1])
+	if result[0] == "お洒落":
 		print(result[0], '\t', result[1])
 	else:
 		pass
